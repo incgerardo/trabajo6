b938a427bbc4c9412a07671e528d65062ed87cf0
incgerardo <incgerardo@gmail.com>
2023-11-16 08:21:14 -0300

    el programa esta listo

--- trabajo6.py
+++ trabajo6.py
@@ -2,23 +2,21 @@ from bs4 import BeautifulSoup
 import requests
 
 def spider(id):
-
-    url=f"https://www.flex.scoopforwork.com/explore?page={id}"	#nombre de la pagina
+    url=f"https://hauslondon.com/collections/all-furniture?page={id}"	#nombre de la pagina
 
     r=requests.get(url)		#pido la pagina r es un objeto
 
     print(r.status_code)
 
-    soup = BeautifulSoup(r.content,"html5lib")		#transforma el html en un árbol de objetos
+    soup = BeautifulSoup(r.content,"html.parser")		#transforma el html en un árbol de objetos
 
-    title = soup.find_all(class_="text-xl")
+    title = soup.find_all(class_="h4")
 
     print(len(title))
 
-    for t in title:
-        print(t.text)
-
+    for i in range(len(title)):
+        print(title[i].text.lstrip().rstrip())
 i=1
 
-for i in range(75):
+for i in range(12):
     spider(i)
